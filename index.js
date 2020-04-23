6daee83b46df0735fb400353a2348c3437c2c3ef
PurpleDragonfly <10940221@qq.com>
2020-04-23 21:01:36 +0800

    update

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
 /*
- * bid-lazy-path-plugin
+ * @Author: dezhao.chen
+ * @Date: 2020-04-22 21:06:04
+ * @LastEditors: dezhao.chen
+ * @LastEditTime: 2020-04-23 20:58:39
+ * @Description: bid-lazy-path-plugin 懒加载文件添加version
  */
+const path = require('path');
 class BidLazyPathPlugin {
     constructor(options) {
         this.options = options;
     }
+
+    //打印 compilation.chunks
+    printChunks(compilation) {
+        compilation.chunks.forEach((chunk) => {
+            // chunk包含多个模块，通过chunk.modulesIterable可以遍历模块列表
+            for (const module of chunk.modulesIterable) {
+                // module包含多个依赖，通过module.dependencies进行遍历
+                module.dependencies.forEach((dependency) => {
+                    console.log('================================================');
+                    console.log(dependency);
+                    console.log('================================================');
+                });
+            }
+        });
+    }
+    // 打印 compilation.assets
+    printAssets(compilation) {
+        for (const name of Object.keys(compilation.assets)) {
+            // compilation.assets[name];
+            // console.log(compilation.assets[name]);
+            console.log(name);
+        }
+    }
+
+    //递归所有assets map对象；找到未未添加版本号的js文件；添加版本号;
+    recursionReplacePath(assets, version) {
+        for (const name of Object.keys(assets)) {
+            if (/\.(js|jsx|ts|tsx)$/.test(name) && name.indexOf(version) == -1) {
+                const newName = this.createNewPath(name, version);
+                assets[newName] = assets[name];
+                delete assets[name];
+            }
+        }
+        return assets;
+    }
+
+    createNewPath(filepath, version) {
+        const dir = path.dirname(filepath);
+        const filename = path.basename(filepath);
+        return [dir, version, filename].join('/');
+    }
+
     apply(compiler) {
         var self = this;
-        // for (var hook of Object.keys(compiler.hooks)) {
-        //     console.log(hook);
-        // }
-        // compiler.plugin('compilation', (compilation) => {
-        //     compilation.plugin('html-webpack-plugin-alter-asset-tags', (data) => {
-        //         data.head = self._handleChunksConfig(data.head, data.plugin.options.version);
-        //         data.body = self._handleChunksConfig(data.body, data.plugin.options.version);
-        //     });
-        // });
-        // compiler.plugin('compilation', (compilation) => {
-        //     const entrypoints = compilation.entrypoints;
-        //     // console.log(compilation);
-        // });
-
-        compiler.plugin('emit', (compilation) => {
+        compiler.hooks.emit.tap('BidLazyPathPlugin', (compilation) => {
             const entrypoints = compilation.entrypoints;
             const points = entrypoints.get('src/p/react-test/0.1.14/index');
-            console.log(compilation.options.output.path);
-            // console.log(points.options);
-            console.log(Object.keys(compilation.assets));
-            // console.log(compilation.assetsInfo);
-            // console.log(Object.keys(compilation.options));
-            // const _assets = compilation.assets.map(key=>{
-            //     if(key.test())
-            // })
+            console.log(this.options);
+            // this.printChunks(compilation);
+            // this.printAssets(compilation);
+            if (this.options.version) {
+                this.recursionReplacePath(compilation.assets, this.options.version);
+            }
         });
-
-        // compiler.hooks.compilation.tap('HtmlWebpackReplaceHost', (compilation) => {
-        //     console.log('compiler.hooks.compilation:', compilation);
-        // });
-        // compiler.hooks.entryOption.tap('html-webpack-replace-host', (compilation) => {
-        //     console.log(compilation);
-        // });
     }
 }
 
